4fb88957db064070cab6cc5509e6da4a185ebfac
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-06-07 09:43:10 +0200

    Add ParameterPartViewHelper

--- Classes/ViewHelpers/Link/ParameterPartViewHelper.php
+++ Classes/ViewHelpers/Link/ParameterPartViewHelper.php
@@ -2,7 +2,6 @@
 
 namespace TRAW\VhsCol\ViewHelpers\Link;
 
-use TYPO3\CMS\Core\LinkHandling\TypoLinkCodecService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
@@ -14,6 +13,7 @@ class ParameterPartViewHelper extends AbstractViewHelper
     public function initializeArguments()
     {
         $this->registerArgument('part', 'string', 'Parameter part to be extracted', true);
+        $this->registerArgument('fallback', 'string', 'Fallback text if parameter part is not set');
     }
 
     /**
@@ -22,7 +22,13 @@ class ParameterPartViewHelper extends AbstractViewHelper
     public function render(): string
     {
         $part = $this->arguments['part'] ?? null;
-        $typoLinkCodec = GeneralUtility::makeInstance(TypoLinkCodecService::class);
-        return $typoLinkCodec->decode($this->renderChildren())[$part] ?? '';
+        if (empty($part)) {
+            return '';
+        }
+
+        $extractedPart = GeneralUtility::makeInstance(\TYPO3\CMS\Frontend\Service\TypoLinkCodecService::class)
+            ->decode($this->renderChildren())[$part] ?? null;
+
+        return $extractedPart ?? $fallback ?? '';
     }
 }
