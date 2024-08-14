29cd5170fbd323a237510dc9ffcaaf7f7f1decb9
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-08-14 10:53:19 +0200

    feat: add argument for link, so the vh can be used as a normal tag throw exception if non-existent part is requested bugfix: fix "empty" part return value

--- Classes/ViewHelpers/Link/ParameterPartViewHelper.php
+++ Classes/ViewHelpers/Link/ParameterPartViewHelper.php
@@ -7,12 +7,34 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
  * Class ParameterPartViewHelper
+ *
+ * Usage:
+ * Inline: {data.header_link->vcol:link.parameterPart(part:'title', fallback:'fallback text goes here')}
+ *
+ * In a condition, if fallback is empty or 0:
+ * <f:if condition="{data.header_link->vcol:link.parameterPart(part:'title', fallback:'')}">
+ * <f:then>Title is set</f:then>
+ * <f:else>Title is not set</f:else>
+ * </f:if>
+ *
+ * As a normal tag: <vcol:link.parameterPart link="{data.header_link}" part="title" fallback="fallback text goes here" />
+ *
+ *
  */
 class ParameterPartViewHelper extends AbstractViewHelper
 {
+    /**
+     * @var array|string[]
+     */
+    protected array $allowedParts = ['url', 'target', 'class', 'title', 'additionalParams'];
+
+    /**
+     * @return void
+     */
     public function initializeArguments()
     {
         $this->registerArgument('part', 'string', 'Parameter part to be extracted', true);
+        $this->registerArgument('link', 'string', 'Link to parameter part');
         $this->registerArgument('fallback', 'string', 'Fallback text if parameter part is not set');
     }
 
@@ -22,13 +44,13 @@ class ParameterPartViewHelper extends AbstractViewHelper
     public function render(): string
     {
         $part = $this->arguments['part'] ?? null;
-        if (empty($part)) {
-            return '';
+        if (empty($part) || !in_array($part, $this->allowedParts)) {
+            throw new \Exception('Given parameter part is not allowed');
         }
 
         $extractedPart = GeneralUtility::makeInstance(\TYPO3\CMS\Core\LinkHandling\TypoLinkCodecService::class)
-            ->decode($this->renderChildren())[$part] ?? null;
+            ->decode($this->arguments['link'] ?? $this->renderChildren())[$part] ?? null;
 
-        return $extractedPart ?? $this->arguments['fallback'] ?? '';
+        return !empty($extractedPart) ? $extractedPart : $this->arguments['fallback'] ?? '';
     }
 }
