4694e1b1667677a25198d96083cb40010d80624a
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-08-14 14:51:54 +0200

    Add attributes for region and format to telephoneviewhelper

--- Classes/ViewHelpers/Uri/TelephoneViewHelper.php
+++ Classes/ViewHelpers/Uri/TelephoneViewHelper.php
@@ -9,30 +9,51 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
  * Class TelephoneViewHelper
+ *
+ * Convert a phone number into a telephone uri
+ *
+ * Usage:
+ * <vcol:uri.telephone phoneNumber="{data.header}" />
+ * <vcol:uri.telephone phoneNumber="{data.header}" defaultRegion="de"/>
+ * <vcol:uri.telephone phoneNumber="{data.header}" defaultRegion="de" format="3"/>
+ *
+ * Inline:
+ * {data.header->vcol:uri.telephone()}
+ * {data.header->vcol:uri.telephone(defaultRegion:'de')}
+ * {data.header->vcol:uri.telephone(defaultRegion:'de',format:3)}
  */
 class TelephoneViewHelper extends AbstractViewHelper
 {
     public function initializeArguments()
     {
-        $this->registerArgument('phoneNumber', 'string', 'The phone number to convert to a link', true, '');
+        $this->registerArgument('phoneNumber', 'string', 'The phone number to convert to a link');
+        $this->registerArgument('defaultRegion', 'string', 'Region that we are expecting the number to be from');
+        $this->registerArgument('format', 'integer', 'Phone number format', false, PhoneNumberFormat::RFC3966);
     }
 
     /**
      * @return string
      */
-    public function render()
+    public function render(): string
     {
-        $phoneUtil = PhoneNumberUtil::getInstance();
-
-        //Phone Number string from database
-        $inputNumberString = $this->arguments['phoneNumber'];
-
         try {
-            $phoneNumber = $phoneUtil->parse($inputNumberString);
-            //format as RFC3966 phone number with "tel:"-prefix
-            return $phoneUtil->format($phoneNumber, PhoneNumberFormat::RFC3966);
+            $inputNumberString = $this->arguments['phoneNumber'] ?? $this->renderChildren();
+            $phoneUtil = PhoneNumberUtil::getInstance();
+            $phoneNumber = $phoneUtil->parse($inputNumberString, $this->arguments['defaultRegion']);
+
+            //if it's already a uri, then we just want the number
+            if (str_starts_with('tel:', $phoneNumber)) {
+                $phoneNumber = substr($phoneNumber, 4);
+            }
+
+            //if we don't know the format, set rfc as format
+            if (!in_array($this->arguments['format'], [0, 1, 2, 3])) {
+                $this->arguments['format'] = 3;
+            }
+
+            return $phoneUtil->format($phoneNumber, $this->arguments['format']);
         } catch (NumberParseException $e) {
-            return '';
+            return $e->getMessage();
         }
     }
 }
