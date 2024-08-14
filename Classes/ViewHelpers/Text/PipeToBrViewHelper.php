9f50e50714924008cf07ec652768ef3b3ff2a44b
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-08-14 14:07:44 +0200

    feat: add options for search and replace attributes, add option to use as inline viewhelper

--- Classes/ViewHelpers/Text/PipeToBrViewHelper.php
+++ Classes/ViewHelpers/Text/PipeToBrViewHelper.php
@@ -4,11 +4,31 @@ namespace TRAW\VhsCol\ViewHelpers\Text;
 
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
+/**
+ * Class PipeToBrViewHelper
+ *
+ * Simple find and replace
+ *
+ * Usage:
+ * <vcol:text.pipeToBr text="{data.header}" />
+ * <vcol:text.pipeToBr text="{data.subheader}" search="***" replace="<br/>"/>
+ *
+ * Inline:
+ * {data.header->vcol:text.pipeToBr()->f:format.raw()}
+ * {data.subheader->vcol:text.pipeToBr(search:'***',replace:'<br/>')->f:format.raw()}
+ *
+ * Typically you would want to wrap the result in f:format.raw
+ */
 class PipeToBrViewHelper extends AbstractViewHelper
 {
+    /**
+     * @return void
+     */
     public function initializeArguments()
     {
-        $this->registerArgument('text', 'string', 'Text to convert', true);
+        $this->registerArgument('text', 'string', 'Text to convert');
+        $this->registerArgument('search', 'string', 'String that should be replaced', false, '|');
+        $this->registerArgument('replace', 'string', 'Replacement', false, '<br>');
     }
 
     /**
@@ -16,6 +36,10 @@ class PipeToBrViewHelper extends AbstractViewHelper
      */
     public function render()
     {
-        return str_replace('|', '<br>', $this->arguments['text']);
+        return str_replace(
+            $this->arguments['search'],
+            $this->arguments['replace'],
+            $this->arguments['text'] ?? $this->renderChildren()
+        );
     }
 }
